5e4b496ac281adadb92d1ea3ff77b681ebc18f65
Steffnomidi <steffnomidi@gmail.com>
2015-09-03 23:42:49 +0500

    https://github.com/steffnomidi/Orange-Tail-coat.git

---
+++ footer.php
@@ -0,0 +1,22 @@
+		<div class="empty"></div>
+	</div>
+	<div class="footer">
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">© Оранжевый фрак, <?php echo date('Y'); ?>.</div>
+					<div class="col-sm-4 text-center">
+						<a href=""><i class="vk"></i></a>
+						<a href=""><i class="vk"></i></a>
+						<a href=""><i class="vk"></i></a>
+					</div>
+					<div class="col-sm-4 text-right"><a href="">Made with <i class="love"></i> in Cosmos</a></div>
+				</div>
+			</div>
+		</footer>
+	</div>
+
+	<?php wp_footer(); ?>
+
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<?php if ( is_front_page() ) { $bodyclass = 'mainpage'; } ?>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- Bootstrap -->
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+	<?php wp_head(); ?>
+
+  <title><?php wp_title(); ?></title> <!-- install and enable plugin "seo yoast" -->
+
+	</head>
+  <body class="<?php echo $bodyclass; ?>">
+	<div class="wrap">
