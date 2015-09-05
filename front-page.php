7130a1153189bf9fae50b502b64e7cbebd1e28ec
Steffnomidi <steffnomidi@gmail.com>
2015-09-05 10:12:13 +0500

    some reorganization

--- front-page.php
+++ front-page.php
@@ -1,5 +1,5 @@
-<?php get_header('mainpage'); ?>
-		<div class="container">
-			<h1>Hello, world!</h1>
-		</div>
-<?php get_footer(); ?>
+<?php get_header('mainpage'); ?>
+		<div class="container">
+			<h1>Hello, world!</h1>
+		</div>
+<?php get_footer(); ?>
