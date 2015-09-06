96c3716de33319956bac0b5f3aaf625e9342d3b0
Steffnomidi <steffnomidi@gmail.com>
2015-09-06 10:28:14 +0500

    evo mainpage

--- front-page.php
+++ front-page.php
@@ -1,17 +1,19 @@
-<?php get_header('mainpage'); ?>
-<div class="rasp300"></div>
-<div class="middleblock">
-	<div class="child">
-
-		<section>
-			<div class="">
-
-						<h1>Hello, world!</h1>
-
-			</div>
-		</section>
-
-	</div>
-	<div class="helper"></div>
-</div>
-<?php get_footer(); ?>
+<?php get_header('mainpage'); ?>
+<div class="rasp300"></div>
+<div class="middleblock">
+	<div class="child">
+
+		<section>
+			<div class="">
+
+				<div class="mainlogo"></div>
+				<p>Организуем яркие праздники для ярких личностей!</p>
+				<button class="playbutton">смотреть видео</button>
+
+			</div>
+		</section>
+
+	</div>
+	<div class="helper"></div>
+</div>
+<?php get_footer(); ?>
